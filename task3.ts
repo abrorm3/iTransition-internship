f12d403ece93a0ccf46d5f6e0d018e535c909021
abrorm3 <abrormukhammadiev@gmail.com>
2023-08-07 03:40:07 +0500

    fixed errors

--- task3.ts
+++ task3.ts
@@ -1,166 +1,156 @@
 import * as crypto from 'crypto';
 import * as readline from 'readline';
 
-const moves = process.argv.slice(2);
-
 class KeyGenerator {
-    generateRandomKey(): Buffer {
-        return crypto.randomBytes(32); // 256 bits
-    }
+  generateRandomKey(): Buffer {
+    return crypto.randomBytes(32); // 256 bits
+  }
 }
 
 class HMACCalculator {
-    calculateHMAC(key: Buffer, data: string): string {
-        const hmac = crypto.createHmac('sha256', key);
-        hmac.update(data);
-        return hmac.digest('hex');
-    }
+  calculateHMAC(key: Buffer, data: string): string {
+    const hmac = crypto.createHmac('sha256', key);
+    hmac.update(data);
+    return hmac.digest('hex');
+  }
 }
 
 class GameRules {
-    private moves: string[];
+  private moves: string[];
 
-    constructor(moves: string[]) {
-        this.moves = moves;
+  constructor(moves: string[]) {
+    this.moves = moves;
+  }
+
+  determineWinner(playerMove: number, computerMove: number): string {
+    const half = (this.moves.length - 1) / 2;
+    if ((computerMove - playerMove + this.moves.length) % this.moves.length <= half) {
+      return 'lose';
+    } else {
+      return 'win';
     }
-    getWinningMove(userMove: string, computerMove: string): string | null {
-        const mid = Math.floor(this.moves.length / 2);
-        const winningMoves = this.moves.slice(mid + 1).concat(this.moves.slice(0, mid));
+  }
 
-        if (winningMoves.includes(computerMove)) {
-            return userMove;
-        } else if (winningMoves.includes(userMove)) {
-            return computerMove;
-        } else {
-            return null;
-        }
-    }
+  generateTable(): string[][] {
+    const table: string[][] = [];
+    table.push(['', ...this.moves]);
 
-    displayHelpTable(): void {
-        const n = this.moves.length;
-        const table: string[][] = [];
-
-        // Initialize table with headers
-        table[0] = ['', ...this.moves];
-
-        // Fill in table with results
-        for (let i = 0; i < n; i++) {
-            table[i + 1] = [this.moves[i], ...this.getResultsForRow(this.moves[i])];
-        }
-
-        // Display table
-        console.log('\nHelp Table:');
-        for (const row of table) {
-            console.log(row.join('\t'));
-        }
+    for (let i = 0; i < this.moves.length; i++) {
+      const row: string[] = [];
+      row.push(this.moves[i]);
+      for (let j = 0; j < this.moves.length; j++) {
+        const result = this.determineWinner(i + 1, j + 1);
+        row.push(result.charAt(0).toUpperCase());
+      }
+      table.push(row);
     }
 
-    private getResultsForRow(userMove: string): string[] {
-        const results: string[] = [];
-        for (const computerMove of this.moves) {
-            if (userMove === computerMove) {
-                results.push('Draw');
-            } else {
-                const winningMove = this.getWinningMove(userMove, computerMove);
-                results.push(winningMove ? `Win (${winningMove})` : 'Lose');
-            }
-        }
-        return results;
-    }
+    return table;
+  }
 }
 
-class Game {
-    private key: Buffer;
-    private userChoice: number;
-    private keyGenerator: KeyGenerator;
-    private hmacCalculator: HMACCalculator;
-    private gameRules: GameRules;
+class RockPaperScissorsGame {
+  private key: Buffer;
+  private moves: string[];
+  private hmacCalculator: HMACCalculator;
+  private gameRules: GameRules;
 
-    constructor() {
-        this.keyGenerator = new KeyGenerator();
-        this.hmacCalculator = new HMACCalculator();
-        this.key = this.keyGenerator.generateRandomKey();
-        this.userChoice = -1;
-        this.gameRules = new GameRules(moves);
-    }
+  constructor(moves: string[]) {
+    this.moves = moves;
+    this.key = new KeyGenerator().generateRandomKey();
+    this.hmacCalculator = new HMACCalculator();
+    this.gameRules = new GameRules(this.moves);
+  }
 
-    async start(): Promise<void> {
-        if (!this.areParametersValid()) {
-            console.log('Error: Incorrect arguments. You must provide an odd number >= 3 of non-repeating strings.');
-            console.log('Example: node script.ts Rock Paper Scissors');
-            return;
-        }
+  private generateComputerMove(): number {
+    return Math.floor(Math.random() * this.moves.length) + 1;
+  }
 
-        this.displayHMAC();
-        await this.getUserChoice();
-        if (this.userChoice !== 0) {
-            this.playGame();
-        }
-    }
+  start(): void {
+    const computerMove = this.generateComputerMove();
+    const computerMoveName = this.moves[computerMove - 1];
 
-    private areParametersValid(): boolean {
-        return moves.length >= 3 && moves.length % 2 === 1 && new Set(moves).size === moves.length;
-    }
+    console.log(`HMAC: ${this.hmacCalculator.calculateHMAC(this.key, computerMoveName)}`);
+    console.log('Available moves:');
+    this.moves.forEach((move, index) => {
+      console.log(`${index + 1} - ${move}`);
+    });
+    console.log('0 - exit');
+    console.log('? - help');
 
-    private displayHMAC(): void {
-        const hmac = this.hmacCalculator.calculateHMAC(this.key, 'HMAC Key');
-        console.log(`HMAC: ${hmac}`);
-    }
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-    private async getUserChoice(): Promise<void> {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        });
-    
-        console.log('Menu:');
-        for (let i = 0; i < moves.length; i++) {
-            console.log(`${i + 1} - ${moves[i]}`);
-        }
-        console.log(`0 - Exit`);
-        console.log(`? - Help`);
-    
-        const input = await new Promise<string>((resolve) => {
-            rl.question('Make your choice: ', resolve);
-        });
-        
+    rl.question('Enter your move: ', (input: string) => {
+      const userInput = input.trim();
+      if (userInput === '0') {
         rl.close();
-    
-        if (input === '?') {
-            this.gameRules.displayHelpTable();
-            await this.getUserChoice();
+        return;
+      }
+
+      if (userInput === '?') {
+        this.displayHelp();
+        rl.question('Enter your move: ', this.handleUserMove.bind(this, computerMove, rl));
+      } else {
+        const playerMove = parseInt(userInput);
+        if (isNaN(playerMove) || playerMove < 1 || playerMove > this.moves.length) {
+          console.log('Invalid input. Please enter a valid move.');
+          this.start();
         } else {
-            const choice = parseInt(input, 10);
-            if (isNaN(choice) || choice < 0 || choice > moves.length) {
-                console.log('Invalid choice. Please select a valid option.');
-                await this.getUserChoice();
-            } else {
-                this.userChoice = choice;
-            }
+          const playerMoveName = this.moves[playerMove - 1];
+          const result = this.gameRules.determineWinner(playerMove, computerMove);
+
+          console.log(`Your move: ${playerMoveName}`);
+          console.log(`Computer move: ${computerMoveName}`);
+          console.log(`You ${result}!`);
+          console.log(`HMAC key: ${this.key.toString('hex').toUpperCase()}`);
+
+          rl.close();
         }
+      }
+    });
+  }
+
+  private displayHelp(): void {
+    const table = this.gameRules.generateTable();
+    console.log('Help Table:');
+    for (const row of table) {
+      console.log(row.join('\t'));
     }
+  }
 
-    private playGame(): void {
-        const computerChoice = Math.floor(Math.random() * moves.length);
-        const userMove = moves[this.userChoice - 1];
-        const computerMove = moves[computerChoice];
-        const hmac = this.hmacCalculator.calculateHMAC(this.key, userMove);
+  private handleUserMove(computerMove: number, rl: readline.Interface, input: string): void {
+    const playerMove = parseInt(input);
+    if (isNaN(playerMove) || playerMove < 1 || playerMove > this.moves.length) {
+      console.log('Invalid input. Please enter a valid move.');
+      rl.question('Enter your move: ', this.handleUserMove.bind(this, computerMove, rl));
+    } else {
+      const playerMoveName = this.moves[playerMove - 1];
+      const result = this.gameRules.determineWinner(playerMove, computerMove);
 
-        console.log(`Your move: ${userMove}`);
-        console.log(`Computer's move: ${computerMove}`);
-        console.log(`HMAC: ${hmac}`);
+      console.log(`Your move: ${playerMoveName}`);
+      console.log(`Computer move: ${this.moves[computerMove - 1]}`);
+      console.log(`You ${result}!`);
+      console.log(`HMAC key: ${this.key.toString('hex').toUpperCase()}`);
 
-        const winningMove = this.gameRules.getWinningMove(userMove, computerMove);
-
-        if (winningMove) {
-            console.log(`You ${winningMove === userMove ? 'win' : 'lose'}!`);
-        } else {
-            console.log(`It's a draw!`);
-        }
-
-        console.log(`Key: ${this.key.toString('hex')}`);
+      rl.close();
     }
+  }
 }
 
-const game = new Game();
-game.start();
+function main(): void {
+  const args = process.argv.slice(2);
+
+  if (args.length < 3 || args.length % 2 !== 1 || new Set(args).size !== args.length) {
+    console.error('Invalid arguments. Please provide an odd number of non-repeating strings.');
+    console.error('Example: node script.ts rock paper scissors lizard Spock');
+    process.exit(1);
+  }
+
+  const game = new RockPaperScissorsGame(args);
+  game.start();
+}
+
+main();
