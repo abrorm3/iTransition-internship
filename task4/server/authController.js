b43a2a4afa6e2671cfef5ddd83f3ea35f889d204
abrorm3 <abrormukhammadiev@gmail.com>
2023-08-11 03:30:52 +0500

    get JWT token

--- task4/server/authController.js
+++ task4/server/authController.js
@@ -1,23 +1,33 @@
 const User = require("./models/User");
 const Role = require("./models/Role");
 const bcrypt = require("bcryptjs");
-const {validationResult} = require("express-validator");
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
+const { secret } = require("./config");
+
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  };
+  return jwt.sign(payload, secret, { expiresIn: "24h" });
+};
 
 class authController {
   async registration(req, res) {
     try {
       const errors = validationResult(req);
-      if(!errors.isEmpty()){
-        return res.status(400).json({message:"Registrator error occured", errors})
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: "Registrator error occured", errors });
       }
       const { email, password } = req.body;
-      const candidate = await User.findOne({email});
+      const candidate = await User.findOne({ email });
       if (candidate) {
         return res.status(400).json({ message: "Email is already registered" });
       }
       const hashPassword = bcrypt.hashSync(password, 7);
       const userRole = await Role.findOne({ value: "USER" });
-      const user = new User({ email,password: hashPassword, roles: [userRole.value] });
+      const user = new User({ email, password: hashPassword, roles: [userRole.value] });
       await user.save();
       return res.json({ message: "Registration successful" });
     } catch (e) {
@@ -27,6 +37,17 @@ class authController {
   }
   async login(req, res) {
     try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(400).json({ message: `Username ${username} not found` });
+      }
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) {
+        return res.status(400).json({ message: `Password or username is not valid` });
+      }
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({token})
     } catch (e) {
       console.log(e);
       res.status(400).json({ message: "Login error" });
