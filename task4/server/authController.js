4344ed425ab52b1b24b2ea58566a08972daad3e7
abrorm3 <abrormukhammadiev@gmail.com>
2023-08-11 03:50:48 +0500

    changed email to username for test

--- task4/server/authController.js
+++ task4/server/authController.js
@@ -20,14 +20,14 @@ class authController {
       if (!errors.isEmpty()) {
         return res.status(400).json({ message: "Registrator error occured", errors });
       }
-      const { email, password } = req.body;
-      const candidate = await User.findOne({ email });
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
       if (candidate) {
-        return res.status(400).json({ message: "Email is already registered" });
+        return res.status(400).json({ message: "Username is already registered" });
       }
       const hashPassword = bcrypt.hashSync(password, 7);
-      const userRole = await Role.findOne({ value: "USER" });
-      const user = new User({ email, password: hashPassword, roles: [userRole.value] });
+      const userRole = await Role.findOne({value: "USER"})
+      const user = new User({ username, password: hashPassword, roles: [userRole.value] });
       await user.save();
       return res.json({ message: "Registration successful" });
     } catch (e) {
@@ -55,11 +55,14 @@ class authController {
   }
   async getUsers(req, res) {
     try {
-      // const userRole = new Role();  TO initialize user roles in mongoDB collections
+      // TO initialize user roles in mongoDB collections
+      // const userRole = new Role();  
       // const adminRole = new Role({value:"ADMIN"})
       // await userRole.save()
       // await adminRole.save()
-      res.json("server works!");
+
+      const users = await User.find()
+      res.json(users);
     } catch (e) {
       console.log(e);
     }
--- task4/server/authRouter.js
+++ task4/server/authRouter.js
@@ -4,7 +4,7 @@ const controller = require('./authController')
 const {check} = require("express-validator")
 
 router.post('/registration', [
-    check("email","Field cannot be empty").notEmpty(),
+    check("username","Field cannot be empty").notEmpty(),
     check("password","Password should be longer than 4 and less than 20 characters").isLength({min:4, max:20}),
 ],controller.registration)
 router.post('/login', controller.login)
